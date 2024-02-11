bb802c2a1fffdc51f686e697bda134f5843afe63
frequem <frequem@github.com>
2024-02-11 19:36:29 +0100

    initial commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#1 SMP PREEMPT Sun Feb 11 18:40:45 CET 2024"
+#define LINUX_COMPILE_BY "frequem"
+#define LINUX_COMPILE_HOST "m3"
+#define LINUX_COMPILER "clang version 16.0.6, LLD 16.0.6"
